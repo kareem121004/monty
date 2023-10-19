e1ce3c0ff646ff99e033965683baad8c5c82acb3
Karim Adel <95775867+kareem121004@users.noreply.github.com>
2023-10-19 18:01:54 +0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,18 +1,10 @@
 #include "monty.h"
 
 /**
- * err - Prints appropiate error messages determined by their error code.
- * @error_code: The error codes are the following:
- * (1) => The user does not give any file or more than one file to the program.
- * (2) => The file provided is not a file that can be opened or read.
- * (3) => The file provided contains an invalid instruction.
- * (4) => When the program is unable to malloc more memory.
- * (5) => When the parameter passed to the instruction "push" is not an int.
- * (6) => When the stack it empty for pint.
- * (7) => When the stack it empty for pop.
- * (8) => When stack is too short for operation.
+ * error1 - Prints appropiate error messages determined by their error code.
+ * @error_code: The error codes
  */
-void err(int error_code, ...)
+void error1(int error_code, ...)
 {
 	va_list ag;
 	char *op;
@@ -47,14 +39,10 @@ void err(int error_code, ...)
 }
 
 /**
- * more_err - handles errors.
- * @error_code: The error codes are the following:
- * (6) => When the stack it empty for pint.
- * (7) => When the stack it empty for pop.
- * (8) => When stack is too short for operation.
- * (9) => Division by zero.
+ * error2 - handles errors.
+ * @error_code: The error codes
  */
-void more_err(int error_code, ...)
+void error2(int error_code, ...)
 {
 	va_list ag;
 	char *op;
